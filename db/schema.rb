7cd5f10f5047591580137c90791a1d86d67c703a
Daniel Schneider <daschne8@gmail.com>
2019-05-13 16:54:52 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,8 @@
 ActiveRecord::Schema.define(version: 1) do
 
   create_table "students", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
+    t.text "first_name"
+    t.text "last_name"
   end
 
 end
